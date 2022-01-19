e4b1911733692f46af86fb8280c59cebb1cbd260
Daniil <dneretin01@gmail.com>
2022-01-19 16:29:02 +0300

    add last page

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -1,17 +1,3 @@
-// export function isWebp() {
-//     function testWebP(callback) {
-//         let webP = new Image();
-//         webP.onload = webP.onerror = function () {
-//             callback(webP.width == 2 && webP.height == 1);
-//         };
-//         webP.src = "data:image/webp;base64,UklGRjIAAABXRUJQVlA4ICYAAACyAgCdASoCAAEALmk0mk0iIiIiIgBoSygABc6zbAAA/v56QAAAAA=="
-//     }
-//
-//     testWebP(function (support) {
-//         let className = support === true ? 'webp' : 'no-webp';
-//         document.documentElement.classList.add(className);
-//     });
-// }
 export function isWebp() {
     function testWebP(callback) {
 

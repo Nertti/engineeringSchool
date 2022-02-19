cc78ef3cf96d2e9713a0beba7fdf5a6032203e09
Daniil <dneretin01@gmail.com>
2022-02-19 20:21:45 +0300

    add user.html and user.scss

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 import * as flsFunctions from './modules/functions.js';
 import * as burg from './modules/burger.js';
 import * as pop from './modules/popup.js';
-import * as func from './modules/justFunc.js';
+// import * as func from './modules/justFunc.js';
 
 flsFunctions.isWebp();
 burg.burger();

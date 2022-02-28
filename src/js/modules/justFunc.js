5b5bddf4d8762379aff8b6457143d9c7db9ccc4a
Daniil <dneretin01@gmail.com>
2022-02-28 20:48:39 +0300

    update docs

---
+++ src/js/modules/justFunc.js
@@ -0,0 +1,7 @@
+export function justFunc() {
+    document.querySelectorAll('.valueNull').forEach(function(elem){
+        if (elem.innerText === 'Отсутствует' || elem.innerText === 'Отсутствуют' || elem.innerText === '0'){
+            elem.style.color = 'red';
+        }
+    })
+}

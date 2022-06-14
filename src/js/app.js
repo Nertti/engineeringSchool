17f02fdb33475a5d4ebbe7a9ddecbd5c7779cb8a
nertti <dneretin01@gmail.com>
2022-06-14 09:29:35 +0300

    финалочка

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import * as flsFunctions from './modules/functions.js';
 import * as burg from './modules/burger.js';
 import * as pop from './modules/popup.js';
 import * as func from './modules/justFunc.js';
+import * as jquery from './modules/jq.js';
 
 flsFunctions.isWebp();
 burg.burger();
